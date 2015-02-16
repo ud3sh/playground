e703605cbef0479abb7d196e8d53f89818ea4a54
Udesh Senaratne <udesh@streetcontxt.com>
2015-02-16 11:52:53 -0500

    Added protcols and enum examples

--- swift-basics/swift-basics.playground/section-1.swift
+++ swift-basics/swift-basics.playground/section-1.swift
@@ -5,10 +5,11 @@ import UIKit
 let myString = "This is a constant string"
 let myInt = 3
 let myDouble = 3.1459
-let planetList = ["Mercury", "Venus", "Earth", "Mars"]
-let myDictionary = ["cat": "Mammal", "ostrich": "Bird"]; //value type is String? (optional)
+let firstTenPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] //this is an array of Int, i.e type [Int]
+let planetList = ["Mercury", "Venus", "Earth", "Mars"] //this is an array of String, i.e type [String]
+let myDictionary = ["cat": "Mammal", "ostrich": "Bird"]; //value type is String? (optional), type Dictionary<String, String>
 let emptyList = []
-let emptyDictionarhy = [:]
+let emptyDictionary = [:]
 
 var myOptionalString:String? = "hello" //{Some 'hello'}
 var letterCountIndex = [String: Int]() //or equivalently Dictionary<String, Int>(), creates an empty map
@@ -22,6 +23,26 @@ println("Alternative way to include values in strings \(myInt + 1)")
 println("My optional string: " + myOptionalString!)
 myOptionalString = "my optional string #2"
 println("My optional string: " + myOptionalString!)
+
+
+//==Strings ==
+var cafe = "cafe";
+var cafe2 = "café";
+
+println(countElements(cafe)) //note cafe.length shouldn't be used as is the number of 16-bit code units rather than the number of unicode chars
+println(countElements(cafe))
+
+if (cafe == cafe2){
+    println("\(cafe) and \(cafe2) are the same")
+} else {
+    println("\(cafe) and \(cafe2) are not the same")
+}
+
+cafe2 += " is closed"
+println(cafe2)
+
+var cafe3 = cafe2; //strings are copied by value in swift, like Int, etc
+println(cafe3)
 
 
 //==Control Structures==
@@ -51,6 +72,8 @@ for (airportCode, city) in airportToCity { //special sytax for iterating through
 for i in 1...3 {  //could use 1..<3 to exclude 3
     println("\(i)th multiple of 7 is \(i*7)")
 }
+
+//note ..., and ..< are range operators
 
 //==Functions
 func greet(name: String) -> String{
@@ -111,3 +134,195 @@ filterList([1, 2, 3, 4, 5],
                 return false
             }
     })
+
+firstTenPrimes.map({n in n * 2}) //double the first ten primes
+firstTenPrimes.map({$0 * 2}) //same as above but the first param is represented by $0 (i.e annonamized)
+
+//--nested functions--
+//  note: local functions cannot refer to themselves (yikes!!!)
+func fib(n:Int) -> Int {
+    func fibIter (target: Int) -> Int {
+        if (target == 0){
+            return 0
+        }
+        var curr = 1;
+        var prev = 0;
+        for i in 1...target {
+            var tmp = curr
+            curr = prev + curr
+            prev = tmp
+        }
+        return prev;
+        
+    }
+    return fibIter(n);
+}
+fib(0)
+fib(1)
+fib(2)
+fib(10)
+
+//==Classes==
+
+class Shape {
+    let numberOfSides: Int
+    init (sides: Int){ //constructor
+        self.numberOfSides = sides; //self is used for this
+    }
+    func getNumberOfSides() -> Int {
+        return self.numberOfSides;
+    }
+    func getDescription() -> String {
+        return "A Shape with \(numberOfSides) sides";
+    }
+}
+
+class Square: Shape { //extends Shape
+    let length: Double
+    init(sideLength: Double) {
+        self.length = sideLength;
+        super.init(sides: 4) //super's init must be called after this classes members are initialized (opposite of Java)
+    }
+    
+    func getArea() -> Double {
+        return length * length;
+    }
+    override func getDescription() -> String {
+        return "Square of length \(length)"
+    }
+}
+let sqr = Square(sideLength: 10)
+println(sqr.getDescription())
+println("A square has \(sqr.numberOfSides) sides")
+
+class MutableEquilateralTriangle: Shape {
+    var length: Double;
+    
+    var perimeter: Double { //variable with getter an setter
+        get {
+            return self.length * 3
+        }
+        set {
+            self.length = newValue / 3.0 //by default newValue is the formalParam name
+        }
+    }
+    init (sideLength: Double) {
+        self.length = sideLength
+        super.init(sides: 3)
+    }
+    
+    func getArea() -> Double {
+        return sqrt(3)/4 * Square(sideLength: self.length).getArea()
+    }
+    
+    override func getDescription() -> String {
+        return "Equilateral triangle of length \(length), having permiter \(perimeter) and area \(getArea())"
+    }
+}
+
+var equilateralTriangle = MutableEquilateralTriangle(sideLength: 3.0);
+println(equilateralTriangle.getDescription())
+equilateralTriangle.perimeter = 7;
+println(equilateralTriangle.getDescription())
+
+
+//==enums
+enum Suit {
+    case SPADE, DIAMOND, HEART, CLUB
+    func description() -> String {
+        switch self {
+        case SPADE: return "spades"
+        case DIAMOND: return "diamonds"
+        case HEART: return "hearts"
+        case CLUB: return "clubs"
+        }
+    }
+}
+
+class Card {
+    let number: Int
+    let suit: Suit
+    init(number:Int, suit: Suit){
+        self.number = number
+        self.suit = suit
+    }
+    func getDescription() -> String {
+        switch self.number {
+        case 1:
+            return "Ace of " + suit.description()
+        case 10:
+            return "Jack of " + suit.description()
+        case 11:
+            return "Queen of " + suit.description()
+        case 12:
+            return "King of " + suit.description()
+        default:
+            return "\(String(self.number)) of \(self.suit.description())"
+        }
+    }
+}
+
+println(Card(number: 1, suit: Suit.DIAMOND).getDescription())
+
+//--enums with parameters--
+enum Response {
+    case Success(String)
+    case Error(String, String)
+}
+
+func handleResponse(response: Response) {
+    switch (response){
+    case let .Error(code, msg): println("Error! \(code) \(msg)")
+    case let .Success(msg): println(msg)
+    }
+}
+let failResponse = Response.Error("401", "Obtaining client list")
+let okResponse = Response.Success("Obtained client list!")
+handleResponse(failResponse)
+handleResponse(okResponse)
+
+
+//==generics==
+
+//==protocols==
+
+protocol EntityWithId {
+    var id: Int {get}
+}
+
+protocol Clonable {
+    typealias T //note: in protocols, generic types must be typealiased
+    mutating func clone()-> T
+}
+
+class Person {
+    let firstName: String
+    let lastName: String
+    var fullName: String {get {return self.firstName + " " + self.lastName}}
+    init(firstName: String, lastName:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    
+}
+
+class ClonablePerson: Person, Clonable, EntityWithId {
+    typealias T = ClonablePerson
+    var id: Int
+    init (id:Int, firstName:String, lastName:String){
+        self.id = id
+        super.init(firstName: firstName, lastName: lastName)
+    }
+    func clone() -> ClonablePerson {
+        return ClonablePerson(id: self.id, firstName: self.firstName, lastName: self.lastName)
+    }
+}
+var clonablePerson = ClonablePerson(id:10, firstName: "Alan", lastName: "Turing")
+println(clonablePerson.fullName)
+println(clonablePerson.clone().fullName)
+
+
+
+
+
